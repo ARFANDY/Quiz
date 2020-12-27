5192c635c1a0216216435e882cf4ab100b87d4c1
Nick Akindinov <akindinovnick@gmail.com>
2020-12-28 00:13:42 +0600

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Никита Акиндинов on 28.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
